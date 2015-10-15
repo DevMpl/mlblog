41605c0c94bb9b64391758384782793ba248efb5
MP harada <harada@mediapl.co.jp>
2015-10-15 19:58:11 +0900

    application helper update

--- app/helpers/mlblog/application_helper.rb
+++ app/helpers/mlblog/application_helper.rb
@@ -7,12 +7,19 @@ module Mlblog
 	      'private' => '非公開',
 	    }
 	  end
-		
 
 	  def get_message_type(type)
 	    types = {"notice" => "success", "error" => "error", "alert" => "error"}
 	    types[type]
 	  end
+		
+		def method_missing method, *args, &block
+      if method.in? Rails.application.routes.url_helpers.instance_methods
+        main_app.send(method, *args)
+      else
+        super
+      end
+    end
 		
   end
 
